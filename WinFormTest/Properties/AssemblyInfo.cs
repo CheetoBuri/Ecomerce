a681989808ebfcf503cc7e04a085cc5d0ead8691
CheetoBuri <khoiphamleanh67@gmail.com>
2025-05-06 02:57:23 -0500

    Added Testing Unit

---
+++ WinFormTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinFormTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinFormTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00ae99ec-4222-451a-a23f-ef6d418154a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WinFormTest/Services/ItemServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BLL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WinFormTest.Services
+{
+    [TestClass]
+    public class ItemServicesTests
+    {
+        private ItemService itemService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            itemService = new ItemService();
+        }
+
+        [TestMethod]
+        public void GetAllItems_ShouldReturnList()
+        {
+            var items = itemService.GetAllItems();
+            Assert.IsNotNull(items);
+            Assert.IsTrue(items.Count >= 0);
+        }
+
+        [TestMethod]
+        public void GetTopSellingItems_ShouldReturnList()
+        {
+            var topItems = itemService.GetTopSellingItems();
+            Assert.IsNotNull(topItems);
+        }
+    }
+}
