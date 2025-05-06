b99fd777e069d80e417e56e5d0e98527a85d3bea
CheetoBuri <khoiphamleanh67@gmail.com>
2025-05-06 05:45:31 -0500

    Added Unit Test Project

---
+++ ASPNETWEBMVCTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ASPNETWEBMVCTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ASPNETWEBMVCTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("960f5c56-921f-4674-9312-7e8f75d4ba4b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ASPNETWEBMVCTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ASPNETWEBMVC.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace ASPNETWEBMVCTests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index_Returns_View()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ViewName == "" ? "Index" : result.ViewName);
+        }
+    }
+}
