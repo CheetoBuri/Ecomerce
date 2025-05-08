cd8deccc573098bb71ffe6f9f40c680a3a247426
CheetoBuri <khoiphamleanh67@gmail.com>
2025-05-07 23:29:30 -0500

    Doing MVC again

--- CoreWebAppMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreWebAppMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
